182e3379ce756287215d3ffc07bb9d3807f82cf7
Francisco Silva <1221518@isep.ipp.pt>
2023-06-09 02:05:11 +0100

    :adhesive_bandage: minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,18 @@
 import { Form } from "@/components/Form";
 import { generateSlug } from "@/utils/generateSlug";
+import { isValidUrl } from "@/utils/isValidUrl";
 import kv from "@vercel/kv";
 import { redirect } from "next/navigation";
 
 export const runtime = "edge";
-
-const isValidUrl = (urlString: string) => {
-  try {
-    return Boolean(new URL(urlString));
-  } catch (e) {
-    return false;
-  }
-};
 
 async function createLink(formData: FormData) {
   "use server";
 
   const url = formData.get("url");
 
-  if (!url || url.toString().length < 5 || !isValidUrl(url.toString()))
-    throw new Error("invalid url");
+  if (!url || !isValidUrl(url.toString()))
+    throw new Error("You've inserted an invalid URL.");
 
   const slug = generateSlug();
 
@@ -30,17 +23,15 @@ async function createLink(formData: FormData) {
 
 export default async function Home() {
   return (
-    <>
-      <div className="flex flex-col gap-4 items-center justify-center min-h-screen">
-        <h1 className="font-semibold text-5xl">tinytie</h1>
-        <h2 className="text-xl text-neutral-400">
-          shorten your links with ease.
-        </h2>
+    <div className="flex flex-col gap-4 items-center justify-center min-h-screen">
+      <h1 className="font-semibold text-5xl">tinytie</h1>
+      <h2 className="text-xl text-neutral-400">
+        shorten your links with ease.
+      </h2>
 
-        <form className="" action={createLink}>
-          <Form />
-        </form>
-      </div>
-    </>
+      <form className="" action={createLink}>
+        <Form />
+      </form>
+    </div>
   );
 }
